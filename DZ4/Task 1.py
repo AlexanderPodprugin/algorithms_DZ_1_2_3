d4d9d41630e0717816bc9ebec0c1d157ac8b999c
Alex Asper <alexpod4321@gmail.com>
2022-12-22 22:26:29 +0300

    DZ4

--- DZ4/Task 1.py
+++ DZ4/Task 1.py
@@ -1,27 +1,27 @@
 def twoCitySchedCost(costs):
-    d_a = []
+    d_a = [] # инициализируем списки 
     d_b = []
     min_sum = 0
-    for i in costs:
-        min_sum += min(i)
-        if i[0] > i[1]:
+    for i in costs: # обход costs
+        min_sum += min(i) #Прибавить i
+        if i[0] > i[1]: #Если 0 элемент списка i больше первого, добавить в список разницу между ними  
             d_b.append(i[0] - i[1])
-        else:
+        else: #Иначе
             d_a.append(i[1] - i[0])
         
-    d_a.sort()
+    d_a.sort() # Сортировка списков
     d_b.sort()
        
-    if len(d_a) > len(d_b):
+    if len(d_a) > len(d_b): # Записываем разницу между ними 
         difference_len = (len(d_a) - len(d_b)) // 2
-        for i in range(difference_len):
-            min_sum += d_a[0]
+        for i in range(difference_len): # Обход
+            min_sum += d_a[0] # к min_sum прибавляем первый элемент и удаляем из списка
             d_a.pop(0)
         
-    elif len(d_a) < len(d_b):
+    elif len(d_a) < len(d_b): # если d_a < d_b, Записываем разницу между ними 
         difference_len = (len(d_b) - len(d_a)) // 2
-        for i in range(difference_len):
-            min_sum += d_b[0]
+        for i in range(difference_len): # обход
+            min_sum += d_b[0] # к min_sum прибавляем первый элемент и удаляем из списка 
             d_b.pop(0)
        
     return min_sum
--- DZ4/Task 2.py
+++ DZ4/Task 2.py
@@ -1,33 +1,65 @@
-def getMinimumDifference(root):
-        if root == None:
-            return 0
-        min_d = 100000
-        stack = [root]
-        while stack:
-            cur_vertex = stack.pop()
-            cur_stack = [cur_vertex]
-            root_vertex_val = cur_vertex.val
-            while cur_stack:
-                cur_vertex_two = cur_stack.pop()
+def getMinimumDifference(root): 
 
-                if cur_vertex_two.left != None:
-                    cur_d = abs(root_vertex_val - cur_vertex_two.left.val)
-                    if cur_d < min_d:
-                        min_d = cur_d
-                    cur_stack.append(cur_vertex_two.left)
-                
-                if cur_vertex_two.right != None:
-                    cur_d = abs(root_vertex_val - cur_vertex_two.right.val)
-                    if cur_d < min_d:
-                        min_d = cur_d
-                    cur_stack.append(cur_vertex_two.right)
-        
-            if cur_vertex.left != None:
-                stack.append(cur_vertex.left)
-            
-            if cur_vertex.right != None:
-                stack.append(cur_vertex.right)
-        
-        return min_d
+        if root == None: #Если пустое вернуть 0 
+
+            return 0 
+
+        min_d = 100000 
+
+        stack = [root] # создаем стак с root 
+
+        while stack: # обход стака 
+
+            cur_vertex = stack.pop()# Получаем вершины 
+
+            cur_stack = [cur_vertex]#записываем в еще один стак вершину и значение в root_vertex_val 
+
+            root_vertex_val = cur_vertex.val 
+
+            while cur_stack: #Обход по второму стаку 
+
+                cur_vertex_two = cur_stack.pop()# получаем вершину 
+
+  
+
+                if cur_vertex_two.left != None: #Если не пустое записываем модуль разницы переменных  
+
+                    cur_d = abs(root_vertex_val - cur_vertex_two.left.val) 
+
+                    if cur_d < min_d: # если cur_d < min_d, то min_d = cur_d 
+
+                        min_d = cur_d 
+
+                    cur_stack.append(cur_vertex_two.left)# добавляем вершину 
+
+                 
+
+                if cur_vertex_two.right != None: #Если не пустое записываем модуль разницы переменных 
+
+                    cur_d = abs(root_vertex_val - cur_vertex_two.right.val) 
+
+                    if cur_d < min_d: # если cur_d < min_d, то min_d = cur_d 
+
+                        min_d = cur_d 
+
+                    cur_stack.append(cur_vertex_two.right) # добавляем вершину 
+
+         
+
+            if cur_vertex.left != None:# Если не пустая переменная, то добавить в стак левую вершину 
+
+                stack.append(cur_vertex.left) 
+
+             
+
+            if cur_vertex.right != None: # Если не пустая переменная, то добавить в стак правую вершину 
+
+                stack.append(cur_vertex.right) 
+
+         
+
+        return min_d 
+
+  
 
         """Сложность O(n)"""
